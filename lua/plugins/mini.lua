ffbbe66697192ef38a175ac401f1f09e30a7efe2
Leo Kamhoot <leo@innovit.co.za>
2025-03-24 10:42:25 +0200

    added a comment plugin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -10,11 +10,27 @@ return { -- Collection of various small independent plugins/modules
     require('mini.ai').setup { n_lines = 500 }
 
     -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
+    require('mini.comment').setup()
     -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
     -- - sd'   - [S]urround [D]elete [']quotes
     -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
+    require('mini.surround').setup {
+      mappings = {
+        -- Toggle comment (like `gcip` - comment inner paragraph) for both
+        -- Normal and Visual modes
+        comment = 'gc',
+
+        -- Toggle comment on current line
+        comment_line = 'gcc',
+
+        -- Toggle comment on visual selection
+        comment_visual = 'gc',
+
+        -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+        -- Works also in Visual mode if mapping differs from `comment_visual`
+        textobject = 'gc',
+      },
+    }
 
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,

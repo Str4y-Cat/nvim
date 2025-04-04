321ee1e871910662a994380ad9d697e374f4a585
Leo Kamhoot <leo@innovit.co.za>
2025-04-04 22:28:10 +0200

    updated todo comments to include MAGIC, a value to locate magic numbers

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,36 +1,42 @@
 -- Standalone plugins with less than 10 lines of config
 return {
-  {
-    -- Auto close paretheses, brackets, quotes, etc..
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true,
-    opts = {},
-  },
-  {
-    -- Creates coloured todo comments.
-    --FIX:
-    --TODO:
-    --HACK:
-    --WARN:
-    --PERF:
-    --NOTE:
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-      signs = true
-    }
+	{
+		-- Auto close paretheses, brackets, quotes, etc..
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		opts = {},
+	},
+	{
+		-- Creates coloured todo comments.
+		--FIX:
+		--TODO:
+		--HACK:
+		--WARN:
+		--PERF:
+		--NOTE:
+		--MAGIC:
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			signs = true,
 
-  },
-  {
-    -- Colour highlighter, for working with colors, aka #ff0032
-    'norcalli/nvim-colorizer.lua',
-    config = function()
-      require('colorizer').setup()
-    end
-  },
+			keywords = {
+				MAGIC = { icon = " ", color = "magic" },
+				PERF = { icon = "󰓅 " },
+			},
 
-
-
+			colors = {
+				magic = { "#7C3AED" },
+			},
+		},
+	},
+	{
+		-- Colour highlighter, for working with colors, aka #ff0032
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
 }

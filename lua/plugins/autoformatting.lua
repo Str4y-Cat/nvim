e22b3c765ac7fa34d519bd94c56bcc29f5d5b7a7
Leo <leo@innovit.co.za>
2025-06-12 08:47:56 +0200

    Added formatting and lsp support for php and glsl

--- lua/plugins/autoformatting.lua
+++ lua/plugins/autoformatting.lua
@@ -1,41 +1,55 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        javascript = { "prettierd" },
-        typescript = { "prettierd" },
-        javascriptreact = { "prettierd" },
-        typescriptreact = { "prettierd" },
-        svelte = { "prettierd" },
-        css = { "prettierd" },
-        scss = { "prettierd" },
-        html = { "prettierd" },
-        json = { "prettierd" },
-        yaml = { "prettierd" },
-        markdown = { "prettierd" },
-        graphql = { "prettierd" },
-        vue = { "prettierd" },
-        lua = { "stylua" },
-        blade = { "blade-formatter" },
-        bash = { "shfmt" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    })
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				svelte = { "prettierd" },
+				css = { "prettierd" },
+				scss = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				yaml = { "prettierd" },
+				markdown = { "prettierd" },
+				graphql = { "prettierd" },
+				vue = { "prettierd" },
+				lua = { "stylua" },
+				blade = { "blade-formatter" },
+				bash = { "shfmt" },
+				php = { "php" },
+				glsl = { "glsl_analyzer" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+			notify_on_error = true,
+			formatters = {
+				php = {
+					command = "php-cs-fixer",
+					args = {
+						"fix",
+						"--rules=@PSR12", -- Formatting preset. Other presets are available, see the php-cs-fixer docs.
+						"$FILENAME",
+					},
+					stdin = false,
+				},
+			},
+		})
 
-    vim.keymap.set({ "n", "v" }, "<leader>f", function()
-      conform.format({
-        lsp_format = true,
-        async = false,
-        timeout_ms = 1000,
-      })
-    end, { desc = "[F]ormat file or range (in visual mode)" })
-  end,
+		vim.keymap.set({ "n", "v" }, "<leader>f", function()
+			conform.format({
+				lsp_format = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "[F]ormat file or range (in visual mode)" })
+	end,
 }

4833c0d28c985bc8b56b5877888375240b8d356b
Leo <leo@innovit.co.za>
2025-07-28 19:37:15 +0200

    Updated lsp to support vue. Fixed folding bug for vue in uft.lua. Readded linting

--- init.lua
+++ init.lua
@@ -5,64 +5,64 @@ require("core.keymaps")
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-  -- global colortheme
-  require("plugins.colortheme"),
+	-- global colortheme
+	require("plugins.colortheme"),
 
-  -- filetree
-  require("plugins.neotree"),
+	-- filetree
+	require("plugins.neotree"),
 
-  -- bottom status line
-  require("plugins.lualine"),
+	-- bottom status line
+	require("plugins.lualine"),
 
-  -- Syntax highlighting
-  require("plugins.treesitter"),
+	-- Syntax highlighting
+	require("plugins.treesitter"),
 
-  -- Fuzzyfinder
-  require("plugins.telescope"),
+	-- Fuzzyfinder
+	require("plugins.telescope"),
 
-  -- Language Server
-  require("plugins.lsp"),
+	-- Language Server
+	require("plugins.lsp"),
 
-  -- Code Completion
-  require("plugins.autocompletion"),
+	-- Code Completion
+	require("plugins.autocompletion"),
 
-  -- Code Formatting
-  require("plugins.autoformatting"),
+	-- Code Formatting
+	require("plugins.autoformatting"),
 
-  -- Code Linting
-  --require("plugins.linting"),
+	-- Code Linting
+	require("plugins.linting"),
 
-  -- Git Intergration
-  require("plugins.gitsigns"),
+	-- Git Intergration
+	require("plugins.gitsigns"),
 
-  -- Cool Intro page
-  require("plugins.alpha"),
+	-- Cool Intro page
+	require("plugins.alpha"),
 
-  -- Indent lines
-  require("plugins.indent-blankline"),
+	-- Indent lines
+	require("plugins.indent-blankline"),
 
-  -- Small Helper functions
-  require("plugins.mini"),
+	-- Small Helper functions
+	require("plugins.mini"),
 
-  -- Key hints
-  require("plugins.which-key"),
+	-- Key hints
+	require("plugins.which-key"),
 
-  -- Method folding
-  require("plugins.ufo"),
+	-- Method folding
+	require("plugins.ufo"),
 
-  -- Directory viewer and editor
-  require("plugins.oil"),
+	-- Directory viewer and editor
+	require("plugins.oil"),
 
-  -- Miscelaneous, less than 10 lines of config
-  require("plugins.misc"),
+	-- Miscelaneous, less than 10 lines of config
+	require("plugins.misc"),
 })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -205,42 +205,92 @@ return {
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
-			-- clangd = {},
-			-- gopls = {},
-			-- pyright = {},
-			-- rust_analyzer = {},
-			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+			--  See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
-			-- Some languages (like typescript) have entire language plugins that can be useful:
-			--    https://github.com/pmizio/typescript-tools.nvim
-			--
-			-- But for many setups, the LSP (`ts_ls`) will work just fine
-			ts_ls = {},
+			-- Vue 3
+			-- volar = {},
+			-- -- TypeScript
 			-- ts_ls = {
-			--   init_options = {
-			--     plugins = {
-			--       {
-			--         name = "@vue/typescript-plugin",
-			--         location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
-			--         languages = { "javascript", "typescript", "vue" },
-			--       },
-			--     },
-			--   },
-			--   filetypes = {
-			--     "javascript",
-			--     "typescript",
-			--     "vue",
-			--   },
+			-- 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			-- 	init_options = {
+			-- 		plugins = {
+			-- 			{
+			-- 				name = "@vue/typescript-plugin",
+			-- 				location = vim.fn.stdpath("data")
+			-- 					.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+			-- 				languages = { "vue" },
+			-- 			},
+			-- 		},
+			-- 	},
 			-- },
+
+			-- Vue 3
+			volar = {
+				init_options = {
+					vue = {
+						hybridMode = false,
+					},
+				},
+				settings = {
+					typescript = {
+						inlayHints = {
+							enumMemberValues = {
+								enabled = true,
+							},
+							functionLikeReturnTypes = {
+								enabled = true,
+							},
+							propertyDeclarationTypes = {
+								enabled = true,
+							},
+							parameterTypes = {
+								enabled = true,
+								suppressWhenArgumentMatchesName = true,
+							},
+							variableTypes = {
+								enabled = true,
+							},
+						},
+					},
+				},
+			},
+			-- TypeScript
+			ts_ls = {
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vim.fn.stdpath("data")
+								.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+							languages = { "vue" },
+						},
+					},
+				},
+				settings = {
+					typescript = {
+						tsserver = {
+							useSyntaxServer = false,
+						},
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+							includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+							includeInlayPropertyDeclarationTypeHints = true,
+							includeInlayFunctionLikeReturnTypeHints = true,
+							includeInlayEnumMemberValueHints = true,
+						},
+					},
+				},
+			},
+
 			html = {},
 			cssls = {},
 			phpactor = {},
 			bashls = {},
 			emmet_ls = {},
 			glsl_analyzer = {},
-
-			--
-
 			lua_ls = {
 				-- cmd = { ... },
 				-- filetypes = { ... },
